109b631a0446e9df338e7d0c0f53867475338917
Grégoire Grzeczkowicz <gregoire@ggrz.fr>
2021-07-06 16:13:37 +0200

    CMakes and .gitignore files

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+	if (argc < 3) {
+		std::cerr << "Usage: " << argv[0] << " Mesh_File PC_File" << std::endl;
+		return 1;
+	}
+
+	std::cout << "Convert " << argv[1] << " into " << argv[2] << std::endl;
+	
+	return 0;
+}
