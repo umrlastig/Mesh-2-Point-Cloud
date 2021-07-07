4a6b7faa0048dfabdda2a046b5872501530c255c
Grégoire Grzeczkowicz <gregoire@ggrz.fr>
2021-07-07 15:16:21 +0200

    Read and write simple Mesh

--- main.cpp
+++ main.cpp
@@ -3,18 +3,23 @@
 #include <OpenMesh/Tools/Utils/getopt.h>
 
 #include <CGAL/Simple_cartesian.h>
-#include <CGAL/Surface_mesh.h>
+#include <CGAL/Point_set_3.h>
+#include <CGAL/property_map.h>
+#include <CGAL/IO/write_ply_points.h>
+#include <CGAL/IO/Color.h>
 
 #include <iostream>
 #include <fstream>
+#include <array>
 
 namespace OpenMesh {
 	typedef TriMesh_ArrayKernelT<>	MyMesh;
 }
 
 namespace CGAL {
-    typedef Simple_cartesian<double>		Kernel;
-    typedef Surface_mesh<Kernel::Point_3>	Mesh;
+	typedef Simple_cartesian<double>			Kernel;
+	typedef Point_set_3<Kernel::Point_3>		Point_set;
+	typedef Point_set::Property_map<Color>	Color_map;
 }
 
 int main(int argc, char *argv[]) {
@@ -38,7 +43,24 @@ int main(int argc, char *argv[]) {
 		return 1;
 	}
 
-	std::cout << mesh.n_vertices() << std::endl;
-	
+	OpenMesh::MPropHandleT< std::map< int, std::string > > property;
+	mesh.get_property_handle(property, "TextureMapping");
+	auto texture_files = mesh.property(property);
+
+	CGAL::Point_set point_set;
+	CGAL::Color_map color;
+
+	boost::tie (color, boost::tuples::ignore) = point_set.add_property_map<CGAL::Color>("color", CGAL::Color(0, 0, 0));
+	auto point = point_set.insert (CGAL::Kernel::Point_3(0., 0., 0.));
+	color[*point] = CGAL::Color(255, 255, 255);
+
+	std::ofstream out_file (argv[2]);
+	CGAL::write_ply_points_with_properties(out_file, point_set,
+										CGAL::make_ply_point_writer (point_set.point_map()),
+										std::make_tuple(color,
+															CGAL::PLY_property<unsigned char>("red"),
+															CGAL::PLY_property<unsigned char>("green"),
+															CGAL::PLY_property<unsigned char>("blue")));
+		
 	return 0;
 }
